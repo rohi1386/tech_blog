fd489b3ecbc10e5a2f63e1e7bc849a56d55c6e8c
rohi1 <rohi1386@yahoo.com>
2022-08-25 01:23:33 +0430

    item hot

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,12 +1,15 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:tech_blog/models/fake_data.dart';
 import 'package:tech_blog/my_colors.dart';
+import 'package:tech_blog/my_string.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+    double bodyMargin = size.width / 17;
     return SafeArea(
       child: Scaffold(
         body: Padding(
@@ -30,7 +33,7 @@ class MainScreen extends StatelessWidget {
               Stack(
                 children: [
                   Container(
-                    width: size.width / 1.10 ,
+                    width: size.width / 1.10,
                     height: size.height / 4.1,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(20)),
@@ -39,7 +42,6 @@ class MainScreen extends StatelessWidget {
                           fit: BoxFit.cover),
                     ),
                     foregroundDecoration: const BoxDecoration(
-
                       borderRadius: BorderRadius.all(Radius.circular(20)),
                       gradient: LinearGradient(
                         colors: GradiantColors.homePosterCoverGradiant,
@@ -48,20 +50,19 @@ class MainScreen extends StatelessWidget {
                       ),
                     ),
                   ),
-
                   Positioned(
                     bottom: 16,
                     left: 0,
                     right: 0,
-                    child:
-                    Column(
+                    child: Column(
                       children: [
                         Padding(
                           padding: const EdgeInsets.only(bottom: 10),
                           child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: [
-                              Text("روح اله عزیر - یک روز پیش",
+                              Text(
+                                "روح اله عزیر - یک روز پیش",
                                 style: TextStyle(
                                   color: SolidColors.posterSubTitle,
                                   fontSize: 12,
@@ -69,14 +70,16 @@ class MainScreen extends StatelessWidget {
                               ),
                               Row(
                                 children: [
-                                  Text("253",
+                                  Text(
+                                    "253",
                                     style: TextStyle(
                                       fontSize: 12,
                                       color: SolidColors.posterSubTitle,
                                     ),
                                   ),
                                   SizedBox(width: 5),
-                                  Icon(CupertinoIcons.eye_solid,
+                                  Icon(
+                                    CupertinoIcons.eye_solid,
                                     color: Colors.white,
                                     size: 15,
                                   ),
@@ -85,8 +88,8 @@ class MainScreen extends StatelessWidget {
                             ],
                           ),
                         ),
-
-                        Text("دوازده قدم برنامه نویسی یک دوره ی...",
+                        Text(
+                          "دوازده قدم برنامه نویسی یک دوره ی...",
                           style: TextStyle(
                             color: SolidColors.posterTitle,
                             fontWeight: FontWeight.bold,
@@ -96,6 +99,124 @@ class MainScreen extends StatelessWidget {
                     ),
                   ),
                 ],
+              ),
+
+              // title in hot article
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 20, 10),
+                child: Row(
+                  children: const [
+                    ImageIcon(
+                        color: SolidColors.colorTitle,
+                        AssetImage("assets/img/hot.png")),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      MyStrings.viewHotestBlog,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w900,
+                        color: SolidColors.colorTitle,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(
+                height: size.height / 3.5,
+                child: ListView.builder(
+                    itemCount: blogList.length,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: ((context, index) {
+                      return
+                        Padding(
+                          padding: EdgeInsets.only(
+                              right: index == 0 ? bodyMargin : 15),
+                          child: Column(
+                          children: [
+                            Stack(children: [
+                              Container(
+                                height: size.height / 5.3,
+                                width: size.width / 2.5,
+                                decoration: BoxDecoration(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(16)),
+                                  image: DecorationImage(
+                                    image: NetworkImage(blogList[index].imageUrl),
+                                    fit: BoxFit.cover,
+                                  ),
+
+                                ),
+                                foregroundDecoration:  BoxDecoration(
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(16)),
+                                  gradient: LinearGradient(colors: GradiantColors.blogPost,
+                                    begin:Alignment.bottomCenter,
+                                    end: Alignment.topCenter,
+
+                                  ),
+
+                                  ),
+                              ),
+                              Positioned(
+                                bottom: 16,
+                                left: 0,
+                                right: 0,
+                                child:
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Text(
+                                      blogList[index].writer,
+                                      style: const TextStyle(
+                                        fontSize: 12,
+                                        color: SolidColors.posterSubTitle,
+                                      ),
+                                    ),
+                                    Row(
+                                      children:  [
+                                        Text(
+                                          blogList[index].views,
+                                          style: TextStyle(
+                                            fontSize: 12,
+                                            color: SolidColors.posterSubTitle,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          width: 3,
+                                        ),
+                                        Icon(
+                                          CupertinoIcons.eye_solid,
+                                          color: Colors.white,
+                                          size: 15,
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ]),
+                            SizedBox(height: 5,),
+                            SizedBox(
+                              width: size.width / 2.4,
+                              child:
+                              Text(blogList[index].title,
+                                  style: TextStyle(
+                                    fontSize: 12
+                                  ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 2,
+                              ),
+
+                            ),
+
+                          ],
+
+                      ),
+                        );
+                    })),
               ),
             ],
           ),
